8fd3cc9ca00d0551a8820e80b93f5c541ba26d8f
Felipe <fconstec@gmail.com>
2025-08-16 17:27:16 -0300

    Ajusta scripts do Front_R10

--- frontend/src/components/ClinicAdminPanel_Managers/PatientForm/PatientProceduresForm/index.ts
+++ frontend/src/components/ClinicAdminPanel_Managers/PatientForm/PatientProceduresForm/index.ts
@@ -1,4 +1,14 @@
-export { default as PatientProceduresForm } from "./PatientProceduresForm";
-export { default as ProcedureRow } from "./ProcedureRow";
-export { default as ProcedureImageGallery } from "./ProcedureImageGallery";
+// Centraliza e padroniza exports do módulo de procedimentos de paciente.
+
+import PatientProceduresForm from "./PatientProceduresForm";
+import ProcedureRow from "./ProcedureRow";
+import ProcedureImageGalleryModal from "./ProcedureImageGalleryModal";
+
+// Exports principais (nomeados)
+export { PatientProceduresForm, ProcedureRow, ProcedureImageGalleryModal };
+
+// Alias para compatibilidade retro (caso o código antigo ainda importe ProcedureImageGallery)
+export { ProcedureImageGalleryModal as ProcedureImageGallery };
+
+// Re-exporta tudo do hook (tipos + hook)
 export * from "./useProcedureForm";
